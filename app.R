0b8e07b91fc03419ea67e02d389d08c762d15ee5
Jonathan Seiden <jonathan.seiden@gmail.com>
2020-09-03 12:13:05 -0400

    Added PII popup, fixed environment data to include all variable names for item-level data

--- app.R
+++ app.R
@@ -1,17 +1,31 @@
-
+# setwd("C:/Users/Jonat/OneDrive - Harvard University/Documents/Git Hub/CREDI Scoring App")
 library(jmscredi)
 library(tidyverse)
 library(ggplot2)
 library(shinybusy)
+library(shinyalert)
 
   ui <- fluidPage(
+    
+    useShinyalert(),
+
+    titlePanel(
+        title=div(img(src="credi_logo.jpg", width = "300px"),
+                  "Scoring Application")
+      ),
+
     sidebarLayout(
       sidebarPanel(
-        #Logo
-        img(src='credi_logo.jpg', align = "middle", width='250px'),
-
+        
+        #Some CSS 
+        tags$head(
+          tags$style(type="text/css", "select {width: 250px; }"),
+          tags$style(type="text/css", ".span4 {width: 290px; }"),
+          tags$style(type="text/css", ".well {width: 300px; }")
+        ),
         #Reverse scoring option
-        checkboxInput("reverse", "Check here if your data IS NOT already reverse-coded", TRUE),
+        checkboxInput("reverse", "My data is NOT reverse-coded", TRUE),
+        checkboxInput("itemlevel", "I want to preserve item-level data", FALSE),
 
         #File upload box
         fileInput("file1", "Choose CSV File",
@@ -32,19 +46,22 @@ library(shinybusy)
       ),
       #The main panel will simply display the processed output
       mainPanel(
+        
+        textOutput("folder"),
 
         add_busy_bar(timeout = 1000, color = "#112446", centered = FALSE,
                      height = "8px"),
 
         conditionalPanel(
           condition = "!output.run",
-          mainPanel(
-                    tags$body("Please upload a CSV. Ensure that your CSV has a unique ID variable, an AGE variable, and CREDI variables. Please see the"),
-                    tags$a(href="https://cdn1.sph.harvard.edu/wp-content/uploads/sites/2435/2016/05/CREDI-Scoring-Manual-8-Jun-2018.pdf",
-                           "CREDI scoring guide"),
-                    tags$body("for more information."),
-          )
-        ),
+          br(),
+          p(tags$body("Please upload a CSV using the sidebar. Ensure that your CSV has a unique ID variable, an AGE variable, and CREDI variables.")),
+          p(tags$body("You can specify if your data is already reverse-coded or not, and if you want to include your item-level data after processing.")),
+          p(tags$body("Please see the",
+            tags$a(href="https://cdn1.sph.harvard.edu/wp-content/uploads/sites/2435/2016/05/CREDI-Scoring-Manual-8-Jun-2018.pdf",
+                   "CREDI scoring guide")),
+            tags$body("for more information."))
+          ),
 
         conditionalPanel(
           condition = "output.success",
@@ -60,8 +77,40 @@ library(shinybusy)
     )
   )
 
-  server <- function(input, output) {
 
+  server <- function(input, output) {
+    
+    #Create a dictionary for CREDI variable names
+    load("environment.rda")
+    
+    vecQnames <- c(mest_df$Item, mest_df$CREDI_code, mest_df$CREDI_code_Apr17)
+
+    #Confirm user will not upload PII
+      shinyalert(
+        title = "Usage Agreement",
+        text = "This app is only to be used with data that DOES NOT include personally identifiable information (PII). By clicking \"I agree\" you are confirming that any data uploaded has been anonymized and does not include any PII or sensitive information." ,
+        closeOnEsc = FALSE,
+        closeOnClickOutside = FALSE,
+        html = FALSE,
+        type = "warning",
+        showConfirmButton = TRUE,
+        showCancelButton = FALSE,
+        confirmButtonText = "I agree",
+        confirmButtonCol = "#228B22",
+        timer = 0,
+        imageUrl = "",
+        animation = TRUE
+      )
+        
+    #Clear data upon session end (maybe not necessary)
+    # session$onSessionEnded(function() {
+    #   if (!is.null(input$file1)) {
+    #     file.remove(input$file1$datapath)
+    #   }
+    # })
+    
+    
+    
     preprocessed <- reactive({
         #ShowDon't throw an error if nothing is uploaded yet.
         validate(
@@ -71,9 +120,14 @@ library(shinybusy)
         #Use the input file name to upload a raw CSV of the results
         inFile <- input$file1
         preprocessed <- readr::read_csv(inFile$datapath)
-
     })
 
+    
+    output$folder <- renderText({
+      inFile <- input$file1
+      inFile$datapath
+    })
+    
     ###Process the data and run the CREDI code, returning a list with the log and the scores (if successful)
     processed <- reactive({
         dat <- jmscredi::score(data = preprocessed(), interactive = FALSE, reverse_code = input$reverse)
@@ -127,7 +181,15 @@ library(shinybusy)
     output$scores <- downloadHandler(
       filename = "Scored_CREDI_Data.csv",
       content = function(file) {
-        write.csv(scores(), file, row.names = FALSE)
+        #We print out all variables if item-level was selected
+        if(input$itemlevel){
+          write.csv(scores(), file, row.names = FALSE)
+        }
+        #Otherwise we remove the item-level data
+        else {
+          scores <- scores()[,!(names(scores()) %in% vecQnames)]
+          write.csv(scores, file, row.names = FALSE)
+        }
       }
     )
 
